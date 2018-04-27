a551a7a22d275fb6f4778fbbed1fc61042f1b124
Sviatlana Zakharenka <sviatlana_zakharenka@epam.com>
2018-04-27 16:10:40 +0300

    refactoring

--- src/test/java/by/htp/testreport/test/LoginTest.java
+++ src/test/java/by/htp/testreport/test/LoginTest.java
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeUnit;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
@@ -16,25 +17,25 @@ import by.htp.testreport.pageobject.MainPage;
 public class LoginTest extends BaseTest {
 
 	private static Logger logger = LogManager.getLogger();
-	//private static Logger logger = LogManager.getRootLogger();
+	// private static Logger logger = LogManager.getRootLogger();
 
-	 @BeforeClass
-	 public WebDriver startBrowser() {
-	
-	 System.setProperty("webdriver.gecko.driver",
-	 "c:\\driver\\geckodriver.exe");
-	 driver = new FirefoxDriver();
-	 driver.manage().window().maximize();
-	 driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-	
-	 return driver;
-	 }
+	@BeforeClass
+	public WebDriver startBrowser() {
+
+		//System.setProperty("webdriver.gecko.driver", "c:\\driver\\geckodriver.exe");
+		System.setProperty("webdriver.chrome.driver", "c:\\driver\\chromedriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		//driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+
+		return driver;
+	}
 
 	@Test
 	public void testLog4j2() {
 
 		List<String> list = null;
-		//Assert.assertNotNull(list);
+		// Assert.assertNotNull(list);
 		logger.info("This is an info message");
 		logger.trace("This is a trace message");
 		logger.debug("This is a debug message");
@@ -42,11 +43,12 @@ public class LoginTest extends BaseTest {
 		logger.warn("This is a warn message");
 		logger.error("This is an error message");
 	}
-	 @Test(enabled = false)
-	 public void doLogin() {
-	
-	 MainPage mainPage = navigate(MainPage.URL);
-	 mainPage.login("tathtp@mail.ru", "Klopik123");
-	 closeBrowser();
-	 }
+
+	@Test(enabled = false)
+	public void doLogin() {
+
+		MainPage mainPage = navigate(MainPage.URL);
+		mainPage.login("tathtp@mail.ru", "Klopik123");
+		closeBrowser();
+	}
 }
